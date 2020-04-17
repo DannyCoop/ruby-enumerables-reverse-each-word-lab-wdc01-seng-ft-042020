c9d1ab598612d0d3ff6235753d9ace1fb3b17440
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-17 09:26:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word array
   solution = ""
-  array.collect do |sample|
-    sample
-  end
   split_array = array.split
   split_array.each do |word|
     solution += word.reverse + " "
